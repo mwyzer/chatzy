134fd07ff2519e0d9f4728193fa08b12477e3e0f
mwyzer <muhammad.wyzer@gmail.com>
2022-12-03 14:26:19 +0700

    add limit 50mb

---
+++ chatzy-backend/src/setupServer.ts
@@ -0,0 +1,66 @@
+import { Application, json, urlencoded, Response, Request, NextFunction, application } from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
+
+
+
+export class chatzyServer {
+    private app: Application;
+
+    constructor(app: Application) {
+        this.app = app;
+    }
+
+    public start(): void {
+        this.securityMiddleware(this.app);
+        this.standardMiddleware(this.app);
+        this.routesMiddleware(this.app);
+        this.globalErrorHandler(this.app);
+        this.startServer(this.app);
+
+    }
+
+    private securityMiddleware(app: Application): void {
+        app.use(
+            cookieSession({
+                name: 'session',
+                keys: ['test1', 'test2'],
+                maxAge: 24 * 7 * 360000,
+                secure: false
+            })
+        );
+        app.use(hpp());
+        app.use(helmet());
+        app.use(
+            cors({
+                origin: '*',
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            })
+        )
+
+    }
+
+    private standardMiddleware(app: Application): void {
+        app.use(compression());
+        app.use(json({ limit: '50mb' }));
+        app.use(urlencoded({ extended: true, limit: '50mb' }));
+    }
+
+    private routesMiddleware(app: Application): void { }
+
+    private globalErrorHandler(app: Application): void { }
+
+    private startServer(app: Application): void { }
+
+    private createSocketID(httpServer: http.Server): void { }
+
+
+}

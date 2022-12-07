24f6da07ab21afa2a890175b9d2d63b847d0f6b3
mwyzer <muhammad.wyzer@gmail.com>
2022-12-08 00:48:24 +0700

    change error

--- chatzy-backend/src/app.ts
+++ chatzy-backend/src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
-import { ChatzyServer } from './setupServer';
-import databaseConnection from './setupDatabase';
-import { config } from './config';
+import { ChatzyServer } from '@root/setupServer';
+import databaseConnection from '@root/setupDatabase';
+import { config } from '@root/config';
 
 class Application {
   public initialize(): void {
---
+++ chatzy-backend/src/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (app) => {
+    const routes = () => {
+        routes();
+        // app.use('/api/v1/')
+    };
+};
+//# sourceMappingURL=routes.js.map
